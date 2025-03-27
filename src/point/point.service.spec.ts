f9ea12cbe4e28356b9f7f2f51e57e006e69a0a37
Harry <harry@bzznbyd.com>
2025-03-27 23:53:17 +0900

    Feat :: 1. getPoint 테스트 작성 및 코드 구현 - 테스트 관련 기능 정의 포함, 2. 경로 문제로 인한 database source import 상대경로 변경

--- src/point/point.service.spec.ts
+++ src/point/point.service.spec.ts
@@ -1,14 +1,18 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { PointService } from './point.service';
-import { UserPointTable } from 'src/database/userpoint.table';
-import { PointHistoryTable } from 'src/database/pointhistory.table';
+import { UserPointTable } from '../database/userpoint.table';
+import { PointHistoryTable } from '../database/pointhistory.table';
 
+
+// describe :: 관련된 테스트 케이스들을 논리적으로 그룹화, 중첩이 가능하여 계층적 테스트 구조를 만들 수 있다.
 describe('PointService', () => {
   let service: PointService;
   let userPointTable: UserPointTable;
   let pointHistoryTable: PointHistoryTable;
 
+  // beforeEach :: 각 테스트 케이스 실행 전에 실행되는 코드
   beforeEach(async () => {
+    // Test.createTestingModule :: NestJS 모듈을 테스트용으로 생성하는 메서드. 의존성 주입을 테스트 하는 환경에서도 활용할 수 있도록 해줌.
     const module: TestingModule = await Test.createTestingModule({
       providers: [
         PointService,
@@ -17,15 +21,36 @@ describe('PointService', () => {
       ]
     }).compile();
 
+    // module.get<T>: 특정 타입의 의존성을 모듈에서 가져오는 메서드
     service = module.get<PointService>(PointService);
     userPointTable = module.get<UserPointTable>(UserPointTable);
     pointHistoryTable = module.get<PointHistoryTable>(PointHistoryTable);
   });
 
+  // it :: 실제 테스트 케이스를 정의. 각 it 블록은 하나의 테스트 케이스를 나타낸다.
   it('should be defined', () => {
     expect(service).toBeDefined();
   });
 
-  // Todo :: 테스트 케이스들을 추가할 예정
+  describe('getPoint', () => {
+    it('사용자 ID로 포인틀르 조회할 수 있어야 한다.', async () => {
+      // Given
+      const userId = 1;
+      const expectedPoint = { id: userId, point: 100, updateMillis: Date.now() };
+      // jest.spyOn :: 특정 객체의 메서드를 모니터링하고 제어할 수 있는 스파이를 생성.
+      // mockResolvedValue :: 비동기 함수가 특정 값으로 해결되도록 모킹.
+      jest.spyOn(userPointTable, 'selectById').mockResolvedValue(expectedPoint);
+
+      // When
+      const result = await service.getPoint(userId);
+
+      // Then
+      // expect :: 특정 값을 검증
+      // toEqual :: 값이 예상한 객체와 동일한지 검증
+      expect(result).toEqual(expectedPoint);
+      // toHaveBeenCalledWith :: 특정 함수가 특정 인자로 호출되었는지 검증
+      expect(userPointTable.selectById).toHaveBeenCalledWith(userId);
+    })
+  })
 })
 
--- src/point/point.service.ts
+++ src/point/point.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@nestjs/common';
-import { UserPointTable } from 'src/database/userpoint.table';
-import { PointHistoryTable } from 'src/database/pointhistory.table';
+import { UserPointTable } from '../database/userpoint.table';
+import { PointHistoryTable } from '../database/pointhistory.table';
 import { PointHistory, TransactionType, UserPoint } from './point.model';
 
 
@@ -14,4 +14,7 @@ export class PointService {
     private readonly pointHistoryTable: PointHistoryTable,
   ) { }
 
+  async getPoint(userId: number): Promise<UserPoint> {
+    return this.userPointTable.selectById(userId);
+  }
 }
